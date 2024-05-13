5cb5969b966559ff8d073c67dd9b0c6db803e3da
elkortby <kortbyoussama@gmail.com>
2024-05-13 22:11:43 +0100

    fix

--- src/features/students/studentsApiEndpoints.ts
+++ src/features/students/studentsApiEndpoints.ts
@@ -1,6 +1,6 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react"
 
-const API = import.meta.env.API_URL;
+const API = import.meta.env.VITE_API_URL;
 
 interface Student {
   id: string
